8852f5d899839a6ba82dca7776a39513dcdd2936
Simon Branford <simon_branford@hotmail.com>
2020-01-21 21:32:02 +0000

    Created IScraper interface, concrete class and first unit test

---
+++ TechTestMVCWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TechTestMVCWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
